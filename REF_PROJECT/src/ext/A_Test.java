8efb9feee2d890fce7dc96b47dbcf3466d9bed65
201-16 <201-16@DESKTOP-SO9VSLM>
2023-09-19 11:13:48 +0900

    2

--- REF_PROJECT/src/ext/A_Test.java
+++ REF_PROJECT/src/ext/A_Test.java
@@ -4,8 +4,11 @@ public class A_Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		new A().a();
+		//A a =(A)new B();
+		C c =(C)new B();
+		//B b =(B)new A();
+		//C c = (C)new A();
+		//new A().a();
 
 	}
 
